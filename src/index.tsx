c6f67e0a6ca3fcf9af183f11f9ca794554779c52
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-06-23 16:04:15 +0300

    web vitals comments cleaned

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,4 @@ const Application: React.FC = () => {
 const root = createRoot(document.getElementById('root')!)
 root.render(<Application />)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(console.log)
